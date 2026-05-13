20b3406402758fd4a8d2334f40d3bbf162d90eb3
Keith Frazer <Frazer.YKeith@gmail.com>
2026-05-13 11:36:39 -0400

    first commit - base markov chain implementation

---
+++ markov.py
@@ -0,0 +1,53 @@
+import numpy as np
+
+class DiscreteChain:
+    def __init__(self, transition_probabilities_matrix: np.array, labels: np.array):
+        """
+            Check for valid TPM
+        """
+        valid_tpm = np.all(np.isclose(np.sum(transition_probabilities_matrix,axis=1),1.0))
+        if not valid_tpm:
+            raise ValueError("Invalid Transition Probabilities Matrix - Rows Must Sum to 1")
+        if transition_probabilities_matrix.shape[0] != transition_probabilities_matrix.shape[1]:
+            raise ValueError("Invalid Transition Probabilities Matrix - Matrix Must be Square")
+        
+        """
+            Check for Label congruence with TPM
+        """
+        if labels.ndim != 1:
+            raise ValueError("Invalid Labels - Must be 1D array")
+        if labels.shape[0] != transition_probabilities_matrix.shape[0]:
+            raise ValueError("Invalid Labels - Must Have Number of Labels Equal to States in TPM")
+
+        self.tpm = transition_probabilities_matrix
+        self.labels = labels
+
+
+    def generate_chain(self, starting_distribution: np.array, chain_length: int) -> np.array:
+        """
+            Generate Chain Based on TPM
+
+            args:
+                chain_length - Length of Chain to generate
+        """
+
+        chain = np.zeros(chain_length, dtype = int)
+        arranged = np.arange(self.tpm.shape[0])
+        
+        rng = np.random.default_rng()
+        current_state = rng.choice(arranged, p = starting_distribution)
+        for i in range(chain_length):
+            chain[i] = current_state
+            current_state = rng.choice(arranged, p = self.tpm[current_state])
+        
+        return self.labels[chain.astype(int)]
+
+    
+    def print_tpm(self) -> None:
+        print(self.tpm)
+
+j=np.array([[.9,.1],[.6,.4]])
+y=np.array(["sa",0])
+t=np.array([0.5,0.5])
+i = DiscreteChain(j,y)
+print(i.generate_chain(t,20))
